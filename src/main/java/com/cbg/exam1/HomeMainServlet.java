5aa834139367b51dccd849892864b80f92577d99
bgchoi205 <choibg205@naver.com>
2021-06-16 21:30:31 +0900

    jsp servlet start

---
+++ src/main/java/com/cbg/exam1/HomeMainServlet.java
@@ -0,0 +1,30 @@
+package com.cbg.exam1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HomeMainServlet")
+public class HomeMainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HomeMainServlet() {
+        super();
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
