26d9dd927222976b6eb7a35a49cfa8379b053efc
bgchoi205 <choibg205@naver.com>
2021-06-16 23:47:48 +0900

    create article/write

--- src/main/java/com/cbg/exam1/servlet/UsrArticleWriteServlet.java
+++ src/main/java/com/cbg/exam1/servlet/UsrArticleWriteServlet.java
@@ -1,6 +1,8 @@
-package com.cbg.exam1;
+package com.cbg.exam1.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,11 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/HomeMainServlet")
-public class HomeMainServlet extends HttpServlet {
+@WebServlet("/usr/article/write")
+public class UsrArticleWriteServlet extends HttpServlet {
 	
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		String name = request.getParameter("name");
 		
 		//들어오는 파라미터를 UTF-8로 해석.
@@ -24,11 +27,12 @@ public class HomeMainServlet extends HttpServlet {
 		// HTML이 UTF-8 형식이라는 것을 브라우저에 알림.
 		response.setContentType("text/html; charset=UTF-8");
 		
-		response.getWriter().append("안녕").append(request.getContextPath());
-		response.getWriter().append(name);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/usr/article/write.jsp");
+		requestDispatcher.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 
 		doGet(request, response);
 	}
