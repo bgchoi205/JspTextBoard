c99409511c3fd5cf056a66dccaf93990037e017c
bgchoi205 <choibg205@naver.com>
2021-06-22 23:12:58 +0900

    getParam 메소드 구현

--- src/main/java/com/cbg/exam1/http/Rq.java
+++ src/main/java/com/cbg/exam1/http/Rq.java
@@ -106,6 +106,22 @@ public class Rq {
 		}
 		
 	}
+
+
+	public String getParam(String paramName, String defaultValue) {
+		String paramValue = req.getParameter(paramName);
+		
+		if(paramValue == null) {
+			return defaultValue;
+		}
+		
+		return paramValue;
+	}
+
+
+	public void printf(String format, Object...args) {
+		print(String.format(format, args));
+	}
 	
 	
 
